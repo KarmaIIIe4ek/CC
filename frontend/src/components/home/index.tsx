16cc547503f7b6c62abea420d76f14a69bf935a2
Nikita <143674989+KarmaIIIe4ek@users.noreply.github.com>
2024-06-23 18:21:06 +0300

    coinGec

--- frontend/src/components/home/index.tsx
+++ frontend/src/components/home/index.tsx
@@ -1,11 +1,17 @@
 import React, {useState, useEffect} from "react";
+import { useAppDispatch } from "../../utils/hook";
+import { getFavoriteAssets } from "../../store/thunks/assets";
 
 
 const Home = () => {
+    const dispatch = useAppDispatch()
+    useEffect(() => {
+        dispatch(getFavoriteAssets('bitcoin'))
+    }, [])
 
     return (
         <div>
-
+            
         </div>
     );
 };
---
+++ frontend/src/store/thunks/assets/index.ts
@@ -0,0 +1,14 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { coinGeckoApi } from "../../../utils/axios";
+
+export const getFavoriteAssets = createAsyncThunk(
+    'coins/markets',
+    async (data: unknown, {rejectWithValue}) => {
+        try {
+            const assets = await coinGeckoApi.get(`coins/markets?vs_currency=usd&ids=${data}`)
+            console.log(assets.data)
+        } catch (error) {
+            
+        }
+    }
+)
