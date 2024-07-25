638dd80537f2da1e3bbee0c446255bb36aecf98a
Nikita <143674989+KarmaIIIe4ek@users.noreply.github.com>
2024-07-26 00:59:56 +0300

    Заполнена главная страница

--- frontend/src/components/main/styles.ts
+++ frontend/src/components/main/styles.ts
@@ -11,7 +11,51 @@ export const useStyles = makeStyles((theme: Theme) => {
             display: 'flex',
             alignItems: 'center',
             justifyContent: 'center',
-            marginTop: '50px !important',
-        }
+            marginTop: '100px !important',
+            flexDirection: 'column',
+        },
+        welcomeText: {
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            fontSize: '40px !important',
+            fontWeight: '500 !important',
+            opacity: '0.8',
+        },
+        bgPadding: {
+            alignItems: 'center',
+            justifyContent: 'center',
+            marginBottom: '10px',
+            paddingBottom: '20px',
+            paddingLeft: '40px',
+            paddingRight: '40px',
+            borderBottom: `1px solid ${colors.borderColor}`,
+        },
+        rates: {
+            marginTop: '50px',
+            alignItems: 'center',
+            justifyContent: 'center',
+        },
+        betweenGraph: {
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: '100%',
+            widht: 'auto',
+            display: 'flex',
+            flexDirection: 'column',
+        },
+        topCardItem: {
+            display: 'flex',
+            backgroundColor: `${theme.palette.mode === 'light' ? colors.primary.DEFAULT : colors.primary[600]}`,
+            padding: '20px 16px',
+            margin: '20px 16px',
+            border: `1px solid ${colors.borderColor}`,
+            borderRadius: 12,
+            justifyContent: 'center',
+            alignItems: 'center',
+            maxWidth: '40%',
+        },
+
+
     })
 })
